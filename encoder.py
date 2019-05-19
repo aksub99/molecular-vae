c465eef70340ee544206fa5dbf80ff8f9e559156
aksub99 <akshaysubramanian99@gmail.com>
2019-05-19 21:42:10 +0530

    Add encoder

---
+++ encoder.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# input size (None, 120, 35)
+class Encoder(nn.Module):
+    
+    def __init__(self, latent_rep_size, max_length = 120, epsilon_std = 0.01):
+        super(Encoder, self).__init__()
+
+        self.latent_rep_size = latent_rep_size
+        self.max_length = max_length
+        self.epsilon_std = epsilon_std
+
+        self.conv_1 = nn.Conv1d(in_channels = 120, out_channels = 9, kernel_size = 9)
+        self.conv_2 = nn.Conv1d(in_channels = 9, out_channels = 9, kernel_size = 9)
+        self.conv_3 = nn.Conv1d(in_channels = 9, out_channels = 10, kernel_size = 11)
+        self.linear_1 = nn.Linear(10*9, 435)
+        self.linear_2 = nn.Linear(435, latent_rep_size)
+        self.linear_3 = nn.Linear(435, latent_rep_size)
+
+        self.relu = nn.ReLU()
+    
+    def sampling(self, args):
+        z_mean_, z_log_var_ = args
+        batch_size = list(z_mean_.shape)[0]
+        epsilon = torch.randn(batch_size, self.latent_rep_size) * self.epsilon_std + 0.
+        return z_mean_ + torch.exp(z_log_var_ / 2) * epsilon
+
+    def vae_loss(self, x, x_decoded_mean): # call this from forward
+        z_mean = self.linear_2(x)
+        z_log_var = self.linear_3(x)
+        x = x.view(x.size()[0], -1)
+        x_decoded_mean = x_decoded_mean.view(x_decoded_mean.size()[0], -1)
+        xent_loss = self.max_length * nn.BCELoss(x, x_decoded_mean)
+        k1_loss = - 0.5 * torch.mean(1 + z_log_var - torch.square(z_mean) - torch.exp(z_log_var), axis = -1)
+        return xent_loss + k1_loss
+
+    def forward(self, x): # run this first
+        x = self.relu(self.conv_1(x)) # (None, 9, 27)
+        x = self.relu(self.conv_2(x)) # (None, 9, 19)
+        x = self.relu(self.conv_3(x)) # (None, 10, 9)
+        x = x.view(x.size()[0], -1) # (None, 90)
+        x = self.relu(self.linear_1(x)) # (None, 435)
+        return(vae_loss, Lambda(sampling, output_shape = (latent_rep_size,), name = 'lambda')([z_mean, z_log_var]))
+
+        
---
+++ model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+from encoder import Encoder
+from decoder import Decoder
+
+class MoleculeVAE(nn.Module):
+    
+    def __init__(self, in_channels, charset, max_length = 120, latent_rep_size = 292):
+        super(MoleculeVAE, self).__init__()
+        charset_length = len(charset)
+        self.encoder = Encoder(latent_rep_size, max_length)
+        self.decoder = Decoder(latent_rep_size, max_length, charset_length)
+
+    def forward(self, x, mode = 'autoencoder'):
+        
+        z_latent = self.encoder(x)
+        decoded_string = self.decoder(z_latent)
+        
+        if mode == 'encoder':
+            return z_latent
+        elif mode == 'decoder':
+            return self.decoder(x)
+        else:
+            return decoded_string
