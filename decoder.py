17e2e2020c37b4b638fe2a0534d986ec1e8d6f33
aksub99 <akshaysubramanian99@gmail.com>
2019-05-25 08:58:50 +0530

    Finish decoder implementation

--- decoder.py
+++ decoder.py
@@ -12,5 +12,22 @@ class Decoder(nn.Module):
         self.max_length = max_length
         self.charset_length = charset_length
 
-        self.linear_1 = nn.Linear(latent_rep_size, latent_rep_size)
-        self.repeat_vector = RepeatVector(self.n)
+        self.linear_1 = nn.Linear(latent_rep_size, latent_rep_size) # (None, latent_rep_size,)
+        self.repeat_vector = RepeatVector(self.max_length) # (None, n, latent_rep_size)
+        self.gru_1 = nn.GRU(latent_rep_size, 501, 3) # (None, n, 501)
+        self.linear_2 = nn.Linear(501, charset_length)
+
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax()
+    
+    def forward(self, x):
+        x = self.relu(self.linear_1(x))
+        x = self.repeat_vector(x)
+        output, hn = self.gru_1(x)
+        output = self.softmax(self.linear_2(output))
+        return output
+
+if __name__ == "__main__":
+    decoder = Decoder(292, 120, 35)
+    x = torch.rand((100, 292,))
+    print(decoder.forward(x))
