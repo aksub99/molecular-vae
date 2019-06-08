304ff4bad0f5011f9350644a2a34f099942d052e
aksub99 <akshaysubramanian99@gmail.com>
2019-06-08 22:45:49 +0530

    Add training loop

--- utils.py
+++ utils.py
@@ -1,5 +1,39 @@
 import torch
 import torch.nn as nn
+import gzip
+import pandas
+import h5py
+import numpy as np
+
+def one_hot_array(i, n):
+    return map(int, [ix == i for ix in xrange(n)])
+
+def one_hot_index(vec, charset):
+    return map(charset.index, vec)
+
+def from_one_hot_array(vec):
+    oh = np.where(vec == 1)
+    if oh[0].shape == (0, ):
+        return None
+    return int(oh[0][0])
+
+def decode_smiles_from_indexes(vec, charset):
+    return "".join(map(lambda x: charset[x], vec)).strip()
+
+def load_dataset(filename, split = True):
+    h5f = h5py.File(filename, 'r')
+    if split:
+        data_train = h5f['data_train'][:]
+    else:
+        data_train = None
+    data_test = h5f['data_test'][:]
+    charset =  h5f['charset'][:]
+    h5f.close()
+    if split:
+        return (data_train, data_test, charset)
+    else:
+        return (data_test, charset)
+
 
 class LambdaLayer(nn.Module):
     def __init__(self, lambd, output_shape, name):
@@ -20,5 +54,4 @@ class RepeatVector(nn.Module):
     def forward(self, x):
         h = x.repeat(self.n, 1)
         return h.view(x.shape[0], -1, x.shape[1])
-
     
